34a40a039ca5076ba7a363d38326936d148e3c12
Peter Aba <peter@peteraba.com>
2019-04-17 15:45:36 +0200

    Real initial commit

---
+++ abter.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use AbterPhp\Framework\Constant\Module;
+
+return [
+    Module::IDENTIFIER         => 'AbterPhp\WebsiteCreative',
+    Module::DEPENDENCIES       => ['AbterPhp\Bootstrap4Website'],
+];
