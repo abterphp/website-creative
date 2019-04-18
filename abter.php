bd42b2d71413ea9ab8941e035f33b20638f910be
Peter Aba <peter@peteraba.com>
2019-04-18 15:33:19 +0200

    Real initial commit with resources

--- abter.php
+++ abter.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 use AbterPhp\Framework\Constant\Module;
 
 return [
-    Module::IDENTIFIER         => 'AbterPhp\WebsiteCreative',
-    Module::DEPENDENCIES       => ['AbterPhp\Bootstrap4Website'],
+    Module::IDENTIFIER    => 'AbterPhp\WebsiteCreative',
+    Module::DEPENDENCIES  => ['AbterPhp\Bootstrap4Website'],
+    Module::ENABLED       => true,
+    Module::RESOURCE_PATH => realpath(__DIR__ . '/resources'),
+    Module::ASSETS_PATHS  => [
+        'creative' => realpath(__DIR__ . '/resources/rawassets'),
+    ],
 ];
