c184d8ab57e509de714a760d24fc978db5091dcd
Peter Aba <peter@peteraba.com>
2019-05-10 10:37:21 +0200

    Code style fixes for abter.php

--- abter.php
+++ abter.php
@@ -6,16 +6,16 @@ use AbterPhp\Framework\Constant\Module;
 use AbterPhp\Framework\Constant\Priorities;
 
 return [
-    Module::IDENTIFIER    => 'AbterPhp\WebsiteCreative',
-    Module::DEPENDENCIES  => ['AbterPhp\Bootstrap4Website'],
-    Module::ENABLED       => true,
-    Module::RESOURCE_PATH => realpath(__DIR__ . '/resources'),
-    Module::MIGRATION_PATHS    => [
+    Module::IDENTIFIER      => 'AbterPhp\WebsiteCreative',
+    Module::DEPENDENCIES    => ['AbterPhp\Bootstrap4Website'],
+    Module::ENABLED         => true,
+    Module::RESOURCE_PATH   => realpath(__DIR__ . '/resources'),
+    Module::MIGRATION_PATHS => [
         Priorities::NORMAL => [
             realpath(__DIR__ . '/src/Databases/Migrations'),
         ],
     ],
-    Module::ASSETS_PATHS  => [
+    Module::ASSETS_PATHS    => [
         'creative' => realpath(__DIR__ . '/resources/rawassets'),
     ],
 ];
